66ffb7a5ad2d703accb9aae776e28e0a0823b54d
Tel Anim <animtel@gmail.com>
2017-10-11 08:20:53 +0300

    Added button of click(event for show time), label of time

--- DuxTeam/DuxTeam/MainPage.xaml.cs
+++ DuxTeam/DuxTeam/MainPage.xaml.cs
@@ -9,9 +9,40 @@ namespace DuxTeam
 {
 	public partial class MainPage : ContentPage
 	{
-		public MainPage()
+        Label label = new Label
+        {
+            VerticalTextAlignment = TextAlignment.Center,
+            HorizontalTextAlignment = TextAlignment.Center,
+            Text = "TIME!",
+            FontSize = Device.GetNamedSize(NamedSize.Large, typeof(Label))
+        };
+        public MainPage()
 		{
 			InitializeComponent();
-		}
-	}
+
+            StackLayout stackLayout = new StackLayout();
+
+            Button button = new Button
+            {
+                Text = "Нажми!",
+                FontSize = Device.GetNamedSize(NamedSize.Large, typeof(Button)),
+                BorderWidth = 1,
+                HorizontalOptions = LayoutOptions.Center,
+                VerticalOptions = LayoutOptions.CenterAndExpand
+            };
+            
+            button.Clicked += OnButtonClicked;
+            stackLayout.Children.Add(button);
+            stackLayout.Children.Add(label);
+
+            this.Content = stackLayout;
+        }
+        private void OnButtonClicked(object sender, System.EventArgs e)
+        {
+            DateTime dateTime = DateTime.UtcNow.Date;
+            string test = DateTime.Now.ToString();
+            label.Text = test;
+            
+        }
+    }
 }
